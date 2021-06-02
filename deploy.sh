98655fc49fbff529d8e71cf349c43976d1366900
hanjiawang <1301671498@qq.com>
2021-06-02 20:16:28 +0800

    配置CI

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@ cd public
 git init
 git add -A
 git commit -m "deploy"
-git remote add origin https://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@github.com/justDemo-hjw/justDemo-hjw.github.io.git
-git push -f origin master
+# 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
+# git remote add origin https://${token}@${address}
+git push -f origin https://${token}@${address} master:master
 cd -
