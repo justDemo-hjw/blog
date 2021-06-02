7fff78a373835842126802619a39de52e6fdfdcb
hanjiawang <1301671498@qq.com>
2021-06-02 20:52:56 +0800

    配置CI

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,5 @@ cd public
 git init
 git add -A
 git commit -m "deploy"
-# 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
-# git remote add origin https://${token}@${address}
 git push -f origin https://${token}@${address} master:master
 cd -
