4495b853ed10f86d18ea45685a4bfd4ef45da369
hanjiawang <1301671498@qq.com>
2021-06-03 00:20:35 +0800

    测试action

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,7 @@ cd public
 git init
 git add -A
 git commit -m "deploy"
+echo ${process.env.TOKEN}
+echo 1111
 git push -f https://${process.env.TOKEN}@github.com/justdemo-hjw/justdemo-hjw.github.io.git master:master
 cd -
